0b019011e0a61399235043c4a5f7521c9cec882e
youyou <961781110@qq.com>
2019-05-11 12:52:08 +0800

    Init a TypeScript version.

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,26 @@
 <template>
-    <section id="app" class="section">
-        <transition :name="routerAnimation">
-            <router-view/>
-        </transition>
-        <TabBar v-if="screenWidth <= 768"/>
-    </section>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
+    </div>
 </template>
 
-<script>
-import TabBar from "@/components/TabBar";
-
-export default {
-    name: "App",
-    data() {
-        return {
-            routerAnimation: "slide-left",
-            screenWidth: 0
-        };
-    },
-    components: {
-        TabBar
-    },
-    mounted() {
-        let that = this;
-        that.screenWidth = window.innerWidth;
-
-        window.onresize = () => {
-            that.screenWidth = window.innerWidth;
-        };
-    },
-    beforeRouteUpdate(to, from, next) {
-        console.log("commit route update.");
-        const toDepth = to.path.split("/").length;
-        const fromDepth = to.path.split("/").length;
-        this.routerAnimation =
-            toDepth < fromDepth ? "slide-right" : "slide-left";
-
-        next();
-    }
-};
-</script>
-
 <style lang="sass">
-    #app
-        position: absolute
-        top: 0
-        right: 0
-        bottom: 0
-        left: 0
-        overflow: auto
+#app 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+#nav 
+  padding: 30px
+  a 
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active 
+      color: #42b983
 </style>
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import delicacy from './modules/delicacy';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {
-        delicacy,
-    }
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
 });
